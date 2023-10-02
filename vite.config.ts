fd476560a4eccbb3a2284c414914c075e841bcf6
HSnils <hrsnils@gmail.com>
2023-10-02 23:03:49 +0200

    fix(build): remove vite-plugin-checker
    causes build to not exit propery

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 import { crx, defineManifest } from "@crxjs/vite-plugin";
-import checker from "vite-plugin-checker";
 import packageJson from "./package.json";
 
 const icons = {
@@ -36,6 +35,5 @@ export default defineConfig({
     crx({
       manifest,
     }),
-    checker({ typescript: true }),
   ],
 });
